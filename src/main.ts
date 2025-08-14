51db34972d5db682d1e4d1ec23b198963320eaec
thiago-burnatt <thiago-burnatt@hotmail.com>
2025-08-14 15:22:59 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import { LiquibaseConfig, Liquibase } from 'liquibase';
-
-const myConfig: LiquibaseConfig = {
-  changeLogFile: './changelog.xml',
-  url: 'jdbc:postgresql://localhost:5433/liquibase_test',
-  username: 'admin',
-  password: 'admin',
-};
-const instance = new Liquibase(myConfig);
-
-export async function doEet() {
-  await instance.status();
-  await instance.update({});
-  // await instance.dropAll();
-}
+// import { LiquibaseConfig, Liquibase } from 'liquibase';
+//
+// const myConfig: LiquibaseConfig = {
+//   changeLogFile: './changelog.xml',
+//   url: 'jdbc:postgresql://localhost:5433/liquibase_test',
+//   username: 'admin',
+//   password: 'admin',
+// };
+// const instance = new Liquibase(myConfig);
+//
+// export async function doEet() {
+//   await instance.status();
+//   await instance.update({});
+//   // await instance.dropAll();
+// }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-  await doEet();
+  // await doEet();
 }
 bootstrap();
