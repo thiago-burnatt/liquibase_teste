e98c392d48f8362cd33e7b94ff5ab52ee506cefe
thiago-burnatt <thiago-burnatt@hotmail.com>
2025-08-14 15:12:36 -0300

    .

---
+++ liquibase-config.ts
@@ -0,0 +1,18 @@
+// import {
+//   LiquibaseConfig,
+//   Liquibase,
+// } from 'liquibase';
+//
+// const myConfig: LiquibaseConfig = {
+//   changeLogFile: './changelog.xml',
+//   url: 'jdbc:postgresql://localhost:5433/liquibase_test',
+//   username: 'admin',
+//   password: 'admin',
+// };
+// const instance = new Liquibase(myConfig);
+//
+// export async function doEet() {
+//   await instance.status();
+//   // await instance.update();
+//   // await instance.dropAll();
+// }
