f9b6aa57ff31a4b9fcc0f17b12090454be7e90c4
thiago-burnatt <thiago-burnatt@hotmail.com>
2025-08-14 15:43:36 -0300

    .

--- liquibase-config.ts
+++ liquibase-config.ts
@@ -1,18 +1,14 @@
-// import {
-//   LiquibaseConfig,
-//   Liquibase,
-// } from 'liquibase';
-//
-// const myConfig: LiquibaseConfig = {
-//   changeLogFile: './changelog.xml',
-//   url: 'jdbc:postgresql://localhost:5433/liquibase_test',
-//   username: 'admin',
-//   password: 'admin',
-// };
-// const instance = new Liquibase(myConfig);
-//
-// export async function doEet() {
-//   await instance.status();
-//   // await instance.update();
-//   // await instance.dropAll();
-// }
+import { LiquibaseConfig, Liquibase } from 'liquibase';
+
+const myConfig: LiquibaseConfig = {
+  changeLogFile: './changelog.xml',
+  url: 'jdbc:postgresql://ep-black-truth-a4our3tg.us-east-1.aws.neon.tech/verceldb?user=default&password=SiQmP9A2BRZO&sslmode=require',
+  username: 'admin',
+  password: 'admin',
+};
+const instance = new Liquibase(myConfig);
+
+export default async function doEet() {
+  await instance.status();
+  await instance.update({});
+}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-import { LiquibaseConfig, Liquibase } from 'liquibase';
-
-const myConfig: LiquibaseConfig = {
-  changeLogFile: './changelog.xml',
-  url: 'jdbc:postgresql://ep-black-truth-a4our3tg.us-east-1.aws.neon.tech/verceldb?user=default&password=SiQmP9A2BRZO&sslmode=require',
-  username: 'admin',
-  password: 'admin',
-};
-const instance = new Liquibase(myConfig);
-
-export async function doEet() {
-  await instance.status();
-  await instance.update({});
-}
+import doEet from '../liquibase-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
