cf699caff5ccb962225792c67040e850648d5a5d
thiago-burnatt <thiago-burnatt@hotmail.com>
2025-08-14 15:54:17 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import doEet from '../liquibase-config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-  doEet();
+  await doEet();
 }
 bootstrap();
